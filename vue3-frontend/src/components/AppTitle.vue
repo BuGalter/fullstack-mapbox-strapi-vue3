5a8e6784c5f07676775d98a4eadd19c833fe9e13
BuGalter <wardoc78@gmail.com>
2023-03-03 21:19:03 +0300

    feat: Added to vue component AppTitle

---
+++ vue3-frontend/src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="conteiner">
+    <h2 class="title">
+      {{ text }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style>
+.conteiner {
+  margin-top: 0;
+  padding-top: 0;
+}
+
+.title {
+  text-align: center;
+}
+</style>
