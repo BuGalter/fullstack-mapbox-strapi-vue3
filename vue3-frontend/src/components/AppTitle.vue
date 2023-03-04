a81b1743901bde639665bd8365558a0d839c2bc7
BuGalter <wardoc78@gmail.com>
2023-03-04 14:40:55 +0300

    feat: Added to vue title for app
    feat: Added text title to app
    feat: Added button component to title
    feat: Added styles conteiner for button and title

--- vue3-frontend/src/components/AppTitle.vue
+++ vue3-frontend/src/components/AppTitle.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="conteiner">
+    <AddButton />
     <h2 class="title">
       {{ text }}
     </h2>
@@ -7,21 +8,35 @@
 </template>
 
 <script>
+import AddButton from './AddButton.vue';
+
 export default {
   name: 'AppTitle',
-  props: {
-    text: String,
+
+  components: {
+    AddButton,
+  },
+
+  data() {
+    return {
+      text: 'Приложение для отображения участков на карте',
+    };
   },
 };
 </script>
 
 <style>
 .conteiner {
+  display: flex;
+  justify-content: space-between;
   margin-top: 0;
   padding-top: 0;
+  width: 90vw;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .title {
-  text-align: center;
+  align-self: center;
 }
 </style>
