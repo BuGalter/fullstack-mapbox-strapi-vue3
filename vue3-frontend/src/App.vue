905a2ea9a4fc46927d62fb05a30e57d61023ba46
BuGalter <wardoc78@gmail.com>
2023-03-04 14:43:13 +0300

    feat: Added to vue in app component logic for draw test plots from api
    feat: Added data for colors, map center, token for access to mapbox
    feat: Added methods for get data from api, getJsonData for convert plot point to correct format,
    function for get random integer
    feat: Added main map to conteiner from api mapbox
    feat: Added navigation control to map
    feat: Added rendering plots from api

--- vue3-frontend/src/App.vue
+++ vue3-frontend/src/App.vue
@@ -1,7 +1,109 @@
-<template></template>
+<template>
+  <AppTitle />
+  <div id="mapConteiner" />
+</template>
 
 <script>
-export default {};
+import mapboxgl from 'mapbox-gl';
+import AppTitle from './components/AppTitle.vue';
+import axios from 'axios';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+  },
+
+  data() {
+    return {
+      accessToken: process.env.VUE_APP_MAPBOX_TOKEN,
+      plots: null,
+      points: null,
+      mapCenter: [43.278971, 19.375222],
+      colors: [
+        '#00008B',
+        '#9400D3',
+        '#00FF00',
+        '#B8860B',
+        '#00FF00',
+        '#F0E68C',
+      ],
+    };
+  },
+
+  methods: {
+    fetchPlots() {
+      axios
+        .get('http://localhost:1337/api/plots?populate=*')
+        .then((response) => {
+          this.plots = response.data.data;
+        })
+        .catch((err) => console.log(err));
+    },
+
+    getGeoJsonData(points) {
+      const geoJsonData = points.map((element) => {
+        return [element.attributes.longitude, element.attributes.latitude];
+      });
+
+      return geoJsonData;
+    },
+
+    randomInteger(min, max) {
+      // случайное число от min до (max+1)
+      let rand = min + Math.random() * (max + 1 - min);
+      return Math.floor(rand);
+    },
+  },
+
+  mounted() {
+    this.fetchPlots();
+
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
+
+    const map = new mapboxgl.Map({
+      container: 'mapConteiner',
+      style: 'mapbox://styles/mapbox/outdoors-v11',
+      center: this.mapCenter,
+      zoom: 1,
+    });
+
+    map.on('load', () => {
+      for (let i = 0; i < this.plots.length; i += 1) {
+        this.points = this.plots[i].attributes.points.data;
+        let coordinates = this.getGeoJsonData(this.points);
+        let plotId = this.plots[i].id.toString();
+        let numberRandomColor = this.randomInteger(0, this.plots.length); 
+
+        map.addSource(plotId, {
+          type: 'geojson',
+          data: {
+            type: 'Feature',
+            geometry: {
+              type: 'Polygon',
+              coordinates: [coordinates],
+            },
+          },
+        });
+
+        map.addLayer({
+          id: plotId,
+          type: 'fill',
+          source: plotId, // reference the data source
+          layout: {},
+          paint: {
+            'fill-color': this.colors[numberRandomColor], // random color fill
+            'fill-opacity': 0.7,
+          },
+        });
+      }
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-right');
+  },
+};
 </script>
 
 <style>
@@ -9,8 +111,12 @@ export default {};
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#mapConteiner {
+  height: 90vh;
+  width: 90vw;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
